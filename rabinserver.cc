8e9d79a8d508d1aea9a6237997767f03bc51f25d
Siddhartha Prasad <Siddhartha.Prasad@tufts.edu>
2016-02-24 16:15:05 -0500

    New structur

---
+++ rabinserver.cc
@@ -0,0 +1,25 @@
+#include "rabinserver.h"
+
+
+RabinServer::RabinServer(int port_) {
+
+    port = port_;
+
+    /* Open connection, etc here */
+
+}
+
+
+int RabinServer::add_blocks(char *file) {
+
+
+     
+
+
+
+    return 1;
+}
+
+
+
+
--- rabinserver.h
+++ rabinserver.h
@@ -1,8 +1,8 @@
 
-#ifndef Server_H
-#define Server_H
+#ifndef RabinServer_H
+#define RabinServer_H
 
-
+#include <functional>
 using namespace std;
 
 /* Opens a connection to a machine, helps maintain a rabin fingerprinting
@@ -17,18 +17,27 @@ class RabinServer
 
     public:
     /* Port to be open at */
-    RabinServer(int port);
+    RabinServer(int port_);
     /* Adds a fles blocks to the hash table */
     void add_blocks(char *file); 
 
     /* Writes a given block to the user */
-    int write_to_client(int block);
+    int write_to_client(int client, int block);
 
 
     private:
 
-    /*Need a hash table here*/
+    std::vector<char *> blocks;
+   
+    /* Rabin hash function here char -> int */
+ 
 
+
+    /* Private class variables go here */  
+
+    int port;
+
+    /***********************************/
 
 }
 

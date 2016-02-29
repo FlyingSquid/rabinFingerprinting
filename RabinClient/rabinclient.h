4543ec3ae3e5a6588c55e7d8ac2e661243584ab2
Siddhartha Prasad <Siddhartha.Prasad@tufts.edu>
2016-02-29 17:21:21 -0500

    Strcture problems

--- RabinClient/rabinclient.h
+++ RabinClient/rabinclient.h
@@ -35,17 +35,33 @@ class RabinClient
 {
 
     public:
-        /* Port at which to be open*/
+        /* Server name and port number*/
         RabinClient(char * hostname, int port_);
         ~RabinServer();
 
-        int request_file(char *file, size_t s); 
+        /* Requests a file from the server
+         *
+         * When it receives blocks back from the server,
+         * it stores the blocks where they need to be stored.
+         *
+         * Then recombines and returns a file
+         *
+         *
+         * Requests to server are made in terms of blocks only
+         *
+         * Server caches only on content here, probably need to cache
+         * on file name toooo
+         *
+         * */
+        char *request_file(char *file_name, size_t s); 
 
+        /* Requests a block from the block cache */
         int request_block(int i);
 
+        /* Establishes a connection to the server */
         int connect_to_server();
 
-
+        /* This should probably eventually be a prvate function */
         unsigned insert_block (char *b, int size);
         
         char *get_block(unsigned b);
@@ -63,8 +79,9 @@ class RabinClient
         /* Simulates a local cache (in memory) */
         std::vector<block *> blocks;
         int portno, sockfd, newsockfd;
-        sockaddr_in serv_addr, cli_addr;
-        socklen_t clilen;
+
+        struct hostent* server;
+        sockaddr_in serv_addr;
 
         unsigned max_size;
         /***********************************/

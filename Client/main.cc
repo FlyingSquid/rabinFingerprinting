6a002e8f5af2bbb6c85c569440b87325bea4d7e8
Siddhartha Prasad <Siddhartha.Prasad@tufts.edu>
2016-03-29 15:57:21 -0400

    Created a simple transfer example using main

--- Client/main.cc
+++ Client/main.cc
@@ -2,18 +2,25 @@
 
 int main (int argc, char *argv[]) {
 
-        if(argc < 3) {
-            cout << "Usage requires hostname and port no" << endl;
+        if(argc < 4) {
+            cout << "Usage requires hostname, port no, file addr" << endl;
         }
         int port = atoi(argv[2]);
         RabinClient *r = new RabinClient(argv[1], port);
         
         int a = r->connect_to_server();
 
-
-        for(int i = 0; i < 2; i++) {        
-            a = r->  receive_file(stdout);
-            cerr << "Received " << a <<" blocks.\n\n"<<endl;
+        FILE *f = fopen(argv[3], "w+");
+        string s1 = argv[3];
+        int go=1;
+        while(go){        
+            a = r->  receive_file(f);
+            fflush(f);
+            //cerr << "Received " << a <<" blocks.\n\n"<<endl;
+            //
+            string s = "firefox 'pwd'/" + s1;
+            system(s1.c_str());
+            cin >> a;
         }
         delete (r);
 
